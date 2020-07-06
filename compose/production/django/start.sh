0bc657dd30961e723f4ca391ee6f48f8b9fc5a71
WanglinLi595 <1156494696@qq.com>
2020-07-06 09:51:29 +0800

    11

--- compose/production/django/start.sh
+++ compose/production/django/start.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn blogproject.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000 --chdir=/app
+gunicorn blogproject.wsgi:application -w 2 -k gthread -b 0.0.0.0:8000 --chdir=/app
